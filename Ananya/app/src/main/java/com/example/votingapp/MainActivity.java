b811055c68bea10a6d907192ba3db1f4d4ada25a
ananya-v <ananya.varshne@gamil.com>
2019-03-09 00:00:25 +0530

    Added my app

---
+++ Ananya/app/src/main/java/com/example/votingapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.votingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView winnerTextView;
+    TextView voteA;
+    TextView voteB;
+
+    int votesOfA=0;
+    int votesOfB=0;
+    int totalVotes=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        winnerTextView = findViewById(R.id.result_text_view);
+        voteA = findViewById(R.id.party_a_vote);
+        voteB = findViewById(R.id.party_b_vote);
+    }
+
+    public void voteForA(View v){
+        votesOfA++;
+        totalVotes++;
+        displayVotes();
+
+        Toast.makeText(this, "You have voted A", Toast.LENGTH_SHORT).show();
+        }
+
+    public void voteForB(View v){
+        votesOfB++;
+        totalVotes++;
+        displayVotes();
+        Toast.makeText(this, "You have voted B", Toast.LENGTH_SHORT).show();
+    }
+
+    public void voteForNone(View v){
+        totalVotes++;
+        displayVotes();
+        Toast.makeText(this, "You have not voted for any of above", Toast.LENGTH_SHORT).show();
+    }
+
+    private void displayVotes(){
+        voteA.setText(String.valueOf(votesOfA)+ "/" + String.valueOf(totalVotes));
+        voteB.setText(String.valueOf(votesOfB)+ "/" + String.valueOf(totalVotes));
+
+        if(votesOfA>votesOfB){
+            winnerTextView.setText("A is Winning");
+        }
+        else if(votesOfB>votesOfA){
+            winnerTextView.setText("B is Winning");
+        }
+        else{
+            winnerTextView.setText("Tie");
+        }
+    }
+
+    
+}
